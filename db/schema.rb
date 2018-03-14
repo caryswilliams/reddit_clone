861c3b4c4f64edca1aa01552c0231da537304615
Carys Williams <cwilliams52093@gmail.com>
2018-03-14 17:59:01 +0000

    Added Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_14_112003) do
+ActiveRecord::Schema.define(version: 2018_03_14_152228) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "games", force: :cascade do |t|
     t.text "title"
