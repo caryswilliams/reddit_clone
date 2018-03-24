37a0f05b2ec83cfe285c53d2c1cd717be7c747d4
Carys Williams <cwilliams52093@gmail.com>
2018-03-24 21:25:17 +0000

    Correct Added Time & By For Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,9 @@
 class CommentsController < ApplicationController
   
+ 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(comment_params.merge(user: current_user))
     redirect_to post_path(@post)
   end
 
